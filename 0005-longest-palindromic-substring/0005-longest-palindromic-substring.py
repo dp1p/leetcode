c31a89a0fd1c5c33f1e9446724e4efe65ff70faa
Daniel <109443146+dp1p@users.noreply.github.com>
2024-09-04 21:05:44 -0700

    Time: 7375 ms (9.46%), Space: 16.5 MB (73.72%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,7 +1,7 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         #use a INCREASING window
-        #if there is a word that is a paladrome of window of 2, then increase size of window and restart
+        #if there is a word that is a paladrome of window of 2, then increase size of window and    restart
         #after iteration of i, then go to the next letter
 
         if len(s) == 1:
@@ -9,12 +9,17 @@ class Solution:
 
         greatestPalindrome = '' #store the empty palidrome
         for i in range(len(s)-1):
-            windowSize = 1 #reset the window size to length of the paldinrome
-            for windowSize in range(1, len(s) - i + 1):
-                window = s[i:i+ windowSize]
-                if window == window[::-1] and len(window) > len(greatestPalindrome):
-                    greatestPalindrome = window
-                    
-                    
+            print(f'ITERATION {i} || GREATEST SIZE CURRENTLY {len(greatestPalindrome)}')
+            for j in range(len(s)):
+                window = s[i:j+1]
+               
+                if len(window) > len(greatestPalindrome):
+                    if window == window[::-1] and len(window) > len(greatestPalindrome):
+                        # print(window)
+                        # print(f'{window} IS PALDROMIC')
+                        greatestPalindrome = window
+                    # else:
+                        # print("NOT PALIDROMIC")
+
         # print(f'the greatest palindrome is: {greatestPalindrome}')
         return greatestPalindrome
