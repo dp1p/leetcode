c6c1dd9aeea719959905e3ad21490318dbcbe454
Daniel <109443146+dp1p@users.noreply.github.com>
2024-09-08 19:04:17 -0700

    Time: 57 ms (12.27%), Space: 17.3 MB (34.66%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,33 +1,36 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        #1. remove spaces 
+        #2. make everything lowercase
+        #3. left and right pointers
+        #left starts at s[0]
+        #right start at len(str)-1 (to prevent out of idx)
+
+        #4. to check left and right 
+        
+        #for i in s:
+            #if len(s) % 2 == 0:  #if even
+        
         newStr = ''
-        
-        s = s.lower()
-        
         for letter in s:
             if letter.isalpha() or letter.isdigit():
                 newStr += letter
-            else:
-                newStr = newStr.replace(letter, ' ')
 
-        if len(newStr) == 0:
-            return True
-
+        newStr = newStr.lower()
         left = 0
         right = len(newStr)-1
+        print(newStr)
 
-        if len(newStr) % 2 == 0:
-            # print(f'right {right}')
-            # print(f'left {left}')
-            while left <= right:
-               
-                if newStr[left] == newStr[right]:
+
+        if len(newStr) % 2 == 0: #if newStr is even
+            while left < right: #where to stop
+                if newStr[left] == newStr[right]: #to check letters
                     left += 1
                     right -= 1
                 else:
                     return False
             return True
-        else:
+        else: #else if its not even
             while left != right:
                 if newStr[left] == newStr[right]:
                     left += 1
@@ -35,3 +38,12 @@ class Solution:
                 else:
                     return False
             return True
+                
+                
+
+
+
+
+
+
+        
