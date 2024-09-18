31f255b6ea08dbcaf33d5b2224439648065bbfb1
Daniel <109443146+dp1p@users.noreply.github.com>
2024-09-17 17:20:06 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,23 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #subtract target from the first num
-        #if the difference is not in the nums arr, continue
-            #we will store our value in a hash map [idx, number]
-        #if the difference is in our nums arr (and a different index)
-            #return nums, difference
-        #if len(nums) == 1 and nums[0] == target
-            #return nums
-        
-        h = {} #to store nums, and if difference is in here, then return it
-        for idx, number in enumerate(nums):
-            difference = target - number #9-7
-            # print(difference)
-            if difference not in h.keys():
-                # print(f'{difference} NOT IN HASHMAP ADDING {number}')
-                h[number] = idx
+        h ={}
+        total_diff = 0
+        for i in range(len(nums)):
+            total_diff = target - nums[i]
+            # h[nums[i]]=i
+            if total_diff in h.keys():
+                return h[total_diff], i
             else:
-                # print(f' {difference} IN HASHMAP')
-                number = h.get(difference)
-                return idx, nums.index(difference)
-                
-        # print(h)
+                h[nums[i]]=i
+            print(h)
+        return False
