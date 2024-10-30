e09d956846ab6cb278b8c362c6ce778a601094be
Daniel Phanachone <109443146+dp1p@users.noreply.github.com>
2024-10-30 10:04:34 -0700

    Time: 98 ms (89.01%), Space: 60.9 MB (90.13%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -1,26 +1,19 @@
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
-        #put it in a hashmap first
-        wins = set() #we dont need to know how many they won, so we will use a set to avoid duplicates
-        loses = {} #we want to know how many they lost
-        result = [[], []] #[0] will be for zero loss, [1] will be for people with one loss
+        #lets first figure out who LOST ZERO TIMES game
+        zeroLoss = set() #assume everyone who has won a game hasnt lost a game YET
+        loss = {} #hashmap to keep track of the loss/ ,
+        oneLoss = set() #we will check the loss hashmap and determine if players have more than one loss
 
-        for i in range(len(matches)): #ITERATE THRU THE MATCHES
-            wins.add(matches[i][0])
-            loses[matches[i][1]] = loses.get(matches[i][1], 0) + 1
-
-        #we will check everuome that have lose first
-        for item in loses.items():
-            if item[1] == 1: #if the player has one loss
-                result[1].append(item[0]) #add it to our result, at position
-
-        #to check who won with zero loses
-        for win in wins:
-            if win not in loses: #as long as they are not in the loses
-                result[0].append(win)
-        # print(result)
+        for player in matches: 
+            zeroLoss.add(player[0]) #we will assume everyone who has won has lost zero matches
+            loss[player[1]] = loss.get(player[1], 0) + 1
+        #once we figure out the losses of the players we will now take a look at them and determine their result
         
-        result[0].sort()
-        result[1].sort()
+        for player, totalLoss in loss.items():
+            if player in zeroLoss: #if the player we assumed in 'zeroLoss' is in LOSS
+                zeroLoss.remove(player)
+            if totalLoss == 1: #if the player has only one loss
+                oneLoss.add(player)
 
-        return result
+        return (sorted(zeroLoss)), (sorted(oneLoss))
