2ff943aaa49ab1978efe8cb1478597f36bb11d0a
Daniel Phanachone <109443146+dp1p@users.noreply.github.com>
2024-10-30 11:18:16 -0700

    Time: 3 ms (23.55%), Space: 16.6 MB (30.99%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,57 +1,27 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        #when it comes to stacking, it uses a last in, first out
-        #we push the next value at the end (to insert)
-        #we pop the last value at the end (to remove)
-        
-        #we need to make a separate array
-        #so we can PUSH to a new array
-        #and if the next value is a closing bracket
-        #we will pop the open bracket in the new array
-        #we can check if the corresponding closing bracket is equal to the open
+        #this is a stack problem because we care about the order and placement
+        #we will store the brackets in a separate arr
+        #stacks are last in, first out
 
-        #answer = []
-        #for char in s:
-            #if char is NOT a closing bracket
-                #answer.append(char)
-            #if char IS a closing bracket
-                #check if opening bracket matches the closing bracket #use dict
-                #answer.pop()
-            
-        #return answer == []
-        #this is for checking the closing bracket is matching with the open bracket 
-        matchClosingWithOpen = { 
+        stack = []
+
+        #then we will make a hashmap so we know which bracket goes to which
+        brackets = {
             '}' : '{',
-            ']' : '[',
-            ')' : '('
+            ')' : '(',
+            ']' : '['
         }
-        answer = []
-        
-        # if len(s) == 1:
-        #     return False
-
-        for char in s:
-            if char in matchClosingWithOpen:
-                if answer and answer[-1] == matchClosingWithOpen[char]: #if there is something in answer and the top of the stack matches with the key value of char..
-                    answer.pop() #pop the last bracket in the arr
-                else: #but what if it doesnt match ?
+        #we make the closing bracket the key because we only care when it is closed to check the value of it
+        for i in range(len(s)):
+            if s[i] not in brackets: #if the bracket is open
+                stack.append(s[i]) #simply append it to the stack
+            else: #if the bracket is closed
+                if stack == []: #if the bracket is closed and the stack is empty
+                    return False
+                elif brackets[s[i]] == stack[-1]: #if the value of (closed bracket) is the last of the stack
+                    stack.pop() #pop the bracket in the stack
+                else:
                     return False
 
-            else:
-                answer.append(char)
-        return answer == []
-
-
-
-
-        #   for char in s:
-        #     if char and char == matchClosingWithOpen[char]: #if there is characters in answer and closing bracket does match with 
-        #         answer.append(char)
-        #     else:
-        #         if answer == []: #what if first char is a closing bracket? return False  
-        #             return False
-        #         if answer[-1] == matchClosingWithOpen[char]: #if last character in answerArr == val of char...
-        #             answer.pop()
-        #         else: #what if the last char does not match with the closing bracket?
-        #             return False
-        # return answer == []
+        return stack == []
