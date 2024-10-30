7791dd2dd430bc3ec1877b5d84c12e53e41d8be3
Daniel Phanachone <109443146+dp1p@users.noreply.github.com>
2024-10-29 20:20:22 -0700

    Time: 226 ms (7.97%), Space: 58.2 MB (97.85%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -8,23 +8,19 @@ class Solution:
         for i in range(len(matches)): #ITERATE THRU THE MATCHES
             wins.add(matches[i][0])
             loses[matches[i][1]] = loses.get(matches[i][1], 0) + 1
-        # print(wins)
-        # print(loses)
+
         #we will check everuome that have lose first
         for item in loses.items():
             if item[1] == 1: #if the player has one loss
                 result[1].append(item[0]) #add it to our result, at position
-        
-        #to check who won
-        for win in wins:
-            if win not in result[1] and win not in loses.keys():
-                result[0].append(win)
-        # print(result)
-        
-        # #then we will check for anyone that has won AND is not in the ban list AND is not the result[1]
-        # for item in wins.items():
-        #     if item[0] not in banlist and item[0] not in result[1]:
-        #         result[0].append(item[0])
 
-        result = [sorted(result[0]), sorted(result[1])]
-        return (result)
+        #to check who won with zero loses
+        for win in wins:
+            if win not in loses: #as long as they are not in the loses
+                result[0].append(win)
+        print(result)
+        
+        result[0].sort()
+        result[1].sort()
+
+        return result
