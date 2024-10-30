7a40548f5aa04f8fcc4e033700daea19e9e9422e
Daniel Phanachone <109443146+dp1p@users.noreply.github.com>
2024-10-30 11:24:30 -0700

    Time: 2 ms (27.63%), Space: 16.6 MB (70.38%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,80 +1,24 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        #STACKING! LIFO (last in, first out)
-        #we will make a empty arr called stack
-        #we will iterate thru s till the very end, and append the char from s in it
-        #if char is open
-            #stack.append(s)
-        #if char is closed (matches in our dict)
-            #if stack[-1] = closeToOpen[char] #we check if stack[-1] is open, and find if it is equal to the value of the closing bracket, which is why we make the opening as a value, and closing bracket as a key
-                #stack.pop()
-                #NOTE WE NEVER APPEND THE CLOSING BRACKET, IT WILL ALWAYS BE OPEN BRACKETS IN THE STACK
+        #this is a stack problem because we care about the order and placement
+        #we will store the brackets in a separate arr
+        #stacks are last in, first out
 
-        closeToOpen = {
-            ')' : '(',   
+        stack = []
+
+        #then we will make a hashmap so we know which bracket goes to which
+        brackets = {
             '}' : '{',
+            ')' : '(',
             ']' : '['
         }
-        stack = [] #LAST IN, FIRST OUT 
+        #we make the closing bracket the key because we only care when it is closed to check the value of it
         for char in s:
-            if char in closeToOpen: #if the character is a closing bracket
-                if stack and stack[-1] == closeToOpen[char]: #if the value of ']' is equal to the first char of the stack (which should be an open)
-                    stack.pop()
-                else:
+            if char not in brackets: #if the bracket is open
+                stack.append(char) #simply append it to the stack
+            else: #if the bracket is closed
+                if stack == [] or brackets[char] != stack[-1]: #if brack is closed but the stack is empty or doesnt equal the last value in the stack
                     return False
-            else: #if character is a open bracket
-                stack.append(char)
+                stack.pop() #pop the bracket in the stack
 
-        return len(stack) == 0 
-
-
-
-
-
-
-
-        #each bracket must be closed
-        #if there is '(' there must be a ')', else false, vice versa
-        #IMPORTANT
-        #left brackets must always be first '(', else false
-        
-        #convert s to a list
-        #store our chars in a hashmap, so if there is our value in there, skip
-        #else do stuff
-
-        #1. determine if a bracket has a closing
-        #two pointer method
-        #pointer1 = 0
-        
-        #for pointer1 in s
-            #for pointer2 in range(pointer1, len(s)):
-                #if char = '('
-                    #find ')'
-                        #if found
-                        #s.pop(char)
-                #elif char = ')'
-                    #return false
-                #elif char = '['
-                    #find ')'
-                        #if found
-                        #s.pop(char)
-                        #...
-        # s = list(s) #will make mutable
-        # print(s)
-        # for pointer1 in range(len(s)):
-        #     print(f'CHARACTER IS {pointer1}, find the closing bracket ---------')
-        #     for pointer2 in range(pointer1, len(s)):
-        #         print(f'character: {s[pointer2]}')
-        #         if s[pointer1] == '(':
-        #             if s.index(')') == True:
-        #                 s.pop(pointer1)
-        #                 s.pop(s.index(')'))
-        #             else:
-        #                 return False
-        # if s == []:
-        #     return True
-        # else:
-        #     return False
-
-
-
+        return stack == []
