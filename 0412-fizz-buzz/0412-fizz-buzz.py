09a3dfc5b773dc6d125ef626d66c3f3bd8a93440
Daniel Phanachone <109443146+dp1p@users.noreply.github.com>
2024-11-02 17:41:38 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -0,0 +1,15 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        answer = []
+        number = 1
+        while len(answer) != n:
+            if number % 3 == 0 and number % 5 == 0: #if the number is divisble by both 3 and 5
+                answer.append('FizzBuzz')
+            elif number % 3 == 0:
+                answer.append('Fizz')
+            elif number % 5 == 0:
+                answer.append('Buzz')
+            else:
+                answer.append(number)
+            number += 1
+        return (answer)
