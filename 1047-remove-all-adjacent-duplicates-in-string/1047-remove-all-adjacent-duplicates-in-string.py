cb4207166ad20f913dfe3f7337bf598ac30a08df
Daniel Phanachone <109443146+dp1p@users.noreply.github.com>
2024-11-03 19:23:48 -0600

    Time: 32 ms (15.79%), Space: 17.7 MB (21.55%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        #we can loop until there is no duplicates
+        #if there is a duplicate, pop it, and, go back to the beginning of s 
+        #OR WE CAN USE A STACK, LAST IN, BUT FIRST OUT
+        #we have to back track, or go back to check the previous iteration before making a choice
+        #we check the FIRST letter, then compare it to the SECOND letter
+        #if both letters are equal, pop it
+        #return stack
+        stack = []
+        for i in range(len(s)):
+            if stack == []: #if stack empty
+                stack.append(s[i]) #place the letter in the empty stack
+            elif stack[-1] == s[i]: #if the last letter of the stack is the same as the next iteration
+                stack.pop()
+            else: #else if they do not equal
+                stack.append(s[i])
+        return "".join(stack)
