f3065d5609a53461737faa6aa447d256fdf0148f
Daniel Phanachone <109443146+dp1p@users.noreply.github.com>
2024-11-10 20:22:15 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,16 +1,14 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        #use hashmap
-        #subtract index from target to get a difference
-        #if difference not in there, append it to hashmap
-        h = {} #store difference
-        for i in range(len(numbers)):
-            difference = target - numbers[i] #
-            if numbers[i] in h: #if the difference is in keys #is 7 in there? #is 2 in there
-                # print(f"{difference} is in hashmap")
-                # print(f"VALUES {difference}, {numbers[i]}")
-                return ([h[numbers[i]]+1, i+1])
-            else:
-                # print(f"{target} - {numbers[i]} != {difference}") #store 7
-                h[difference] = i
-                # print(h)
+        left = 0
+        right = 1
+        while left != len(numbers)-1:
+            if left == right: #if both pointers are at the same index
+                right += 1
+            elif numbers[left] + numbers[right] == target:
+                return sorted([left+1, right+1])
+            elif right == len(numbers)-1: #if right has reached the end of the array
+                right = 0
+                left += 1
+            else: #if they do not sum up to the target
+                right += 1
