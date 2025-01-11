56af1b0e9d8db57d3121dca8bed9079e1ee37a35
Daniel Phanachone <109443146+dp1p@users.noreply.github.com>
2025-01-10 20:44:33 -0600

    Time: 3 ms (68.16%), Space: 50.7 MB (53.46%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -16,12 +16,29 @@
  
  */
 var twoSum = function(nums, target) {
-    
-    for (let i = 0; i < nums.length; i++){
-        for (let j = i + 1; j < nums.length; j++){
-            if (nums[i] + nums[j] === target){
-                return [i, j]
-            }
+    const hashmap = {};
+    for (let i = 0; i < nums.length; i++){ 
+        var difference = target - nums[i] 
+        if (!(difference in hashmap)){ 
+            hashmap[nums[i]] = i
+        } else { // 
+            return [hashmap[difference], i] 
         }
-    }
+    } 
+   
 };
+
+
+
+
+
+
+ // for (let i = 0; i < nums.length; i++){
+    //     for (let j = i + 1; j < nums.length; j++){
+    //         if (nums[i] + nums[j] === target){
+    //             return [i, j]
+    //         }
+    //     }
+    // }
+
+    // hashmaps can be searched by O(1)
