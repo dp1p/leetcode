faa298017e23d27875156f9aadaf3944577741ef
Daniel Phanachone <109443146+dp1p@users.noreply.github.com>
2025-02-21 22:46:56 -0600

    Time: 7 ms (81.42%), Space: 31.7 MB (30.3%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,10 +1,3 @@
 class Solution:
-    def containsDuplicate(self, nums):
-        h = {}
-        for idx, val in enumerate(nums):
-            if val not in h.keys():
-                h[val] = []
-                h[val].append(idx)
-            else:
-                return True
-        return False
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(set(nums)) != len(nums)
