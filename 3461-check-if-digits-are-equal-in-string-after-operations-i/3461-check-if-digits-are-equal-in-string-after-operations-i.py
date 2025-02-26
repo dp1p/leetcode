6dda81e410892f800c3068c0f639682e7fa8b952
Daniel Phanachone <109443146+dp1p@users.noreply.github.com>
2025-02-26 01:26:50 -0600

    Time: 54 ms (78.5%), Space: 17.7 MB (80.06%) - LeetHub

--- 3461-check-if-digits-are-equal-in-string-after-operations-i/3461-check-if-digits-are-equal-in-string-after-operations-i.py
+++ 3461-check-if-digits-are-equal-in-string-after-operations-i/3461-check-if-digits-are-equal-in-string-after-operations-i.py
@@ -3,17 +3,19 @@ class Solution:
         #use a while loop 
         #recurisvely calculate until they equal to double digits that are the same digits
         #first calulate all idxs 
+
+
         s = list(s) #<- store it here
-        numbers = []
+        new_numbers = []
         i = 0
         while i < len(s) - 1:
-            numbers.append((int(s[i]) + int(s[i+1])) % 10)
-            i += 1 
-            if i == len(s) - 1: #we check if numbers is a length of two tho
-                if i == 2:
-                    i = 0
-                    print(numbers)
-                    return numbers[i] == numbers[i + 1]
-                s = (numbers) #S IS NOW NUMBER
-                i = 0 #I IS RESET
-                numbers = []
+            new_numbers.append((int(s[i]) + int(s[i+1])) % 10) #calculate
+            i += 1 #iterate
+            if i == len(s) - 1: #if i has reached the very end
+                if i == 2:#we check if numbers is a length of two 
+                    return new_numbers[0] == new_numbers[1]
+                
+                #else reset
+                s = (new_numbers) #S IS NOW NUMBER
+                i = 0 #i IS RESET
+                new_numbers = [] #numbers is reset
