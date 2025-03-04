d7208a257ef1142ddabe1af10a7b4aa834ebc30e
Daniel Phanachone <109443146+dp1p@users.noreply.github.com>
2025-03-03 21:11:23 -0600

    Time: 169 ms (27.18%), Space: 78.6 MB (46.33%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,66 +1,90 @@
-class LRUCache:
-    # all operations must run o(1)
-    # hashmaps or hashsets
-    def __init__(self, capacity: int):
-        # intializes the positive size capacity (the size of the hashmap)
-        self.capacity = capacity
-        self.hashmap = {}
-        self.tracker = []
+class Node:
+    def __init__(self, key, value):
+    #reason why we place a key value is to delete the node when capacity is reached
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
 
-        #use array
-        #depending on the order, we pop
-        # we want to shift that arr
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.node_map = {}
+        self.head = Node(-1, -1)
+        self.tail = Node(-1, -1) 
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _remove(self, node):
+        next_node = node.next 
+        prev_node = node.prev 
+        prev_node.next = next_node
+        next_node.prev = prev_node
+
+    def _add_to_front(self, node):
+        #H <-> N2 <-> N1 <-> N2 <-> T
+        #H <-> N  
+        #H <-> N2 <-> N1 <-> T
+        node.prev = self.head #to point to our dummy head
+        node.next = self.head.next #to point whatever our head was pointing
+        self.head.next.prev = node #have next of head
+        self.head.next = node
+
 
     def get(self, key: int) -> int:
-        # simply search if the key exists
-        # use a tracker to keep track of what has been recently used
-        # print(f'getting')
-        if key in self.hashmap:
-            #replace the order, and move that key to the beginning
-            #shift arr
-
-            self.tracker.pop(self.tracker.index(key))
-            self.tracker.insert(0, key)
-
-            # print(self.tracker)
-            return self.hashmap[key]
-        # print(f'{key} was not found')
+        if key in self.node_map:
+            node = self.node_map[key]
+            self._remove(node)
+            self._add_to_front(node)
+            return node.value
         return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.hashmap:
-            # print('updating')
-            self.hashmap[key] = value
-            self.tracker.pop(self.tracker.index(key))
-            self.tracker.insert(0, key)
+        if key in self.node_map:
+            self._remove(self.node_map[key]) #move the key 
+        
+        node = Node(key, value)
+        self.node_map[key] = node
+        self._add_to_front(node)
 
-        elif len(self.hashmap) != self.capacity:
-            # print('inserting')
-            self.tracker.insert(0, key)
-            self.hashmap[key] = value
-            # print(self.tracker)
-            # print(self.hashmap)
-        else:
-            
-            # in the tracker, determine what has been used the least
-            
-            #insertion takes precedence
-            #we should implement a lastest insertion tracker 
-            key_to_delete = self.tracker.pop() # o(1)
-            del self.hashmap[key_to_delete]
-            # print(f'deleting {key_to_delete}')
-            # print(self.tracker)
-            self.hashmap[key] = value
-            self.tracker.insert(0, key)
+        if len(self.node_map) > self.capacity:
+            lru_node = self.tail.prev
+            self._remove(lru_node)
+            del self.node_map[lru_node.key] 
+        
 
 
 
-        # pair key value
-        # if the key exists, update value
-        # if number keys exceeds capacity, evict the least recently used key
-        # counter how many tiems each one has been called
-        # we can determine the least recently used key by comparing values
-        # sort by key value pairs #lambda function
+        #if key is in hashmap
+        #   update key value pair
+        #   update it so it is the most recently used
+        #   we update it by removing the node, and readding it, then updating the dictionary
+        #if capacity has been reached
+        #   delete least recently used key
+    
+    #add and remove function for the linkedlist
+    #this function will add a linkedlist 
+    #_add(self, node)
+    # pointing our node to the head
+    #node.next = self.head.prev (want our node to where the head is, but it is head.next because our head is a dummy node)
+    #node.prev = self.head #to point to our dummy node
+    #insert our node into our doubly linkedlist
+    #self.head.next.prev = node #head is a dummy node
+    #self.head.next = node
+    
+
+    #h <-> 1 <----> 3
+
+    #_remove(self, node):
+    # prev_node = node.prev
+    # next_node = node.next
+    # prev_node.next = next_node 
+    # next_node.prev = prev_node
+
+
+
+
 
 
 # Your LRUCache object will be instantiated and called as such:
