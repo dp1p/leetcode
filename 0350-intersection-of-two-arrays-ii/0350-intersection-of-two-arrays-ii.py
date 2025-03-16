6a69a2649f541e8409a46a5e4b799d06a3d233af
Daniel Phanachone <109443146+dp1p@users.noreply.github.com>
2025-03-16 15:55:27 -0500

    Time: 0 ms (100%), Space: 17.8 MB (74.21%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -4,23 +4,23 @@ class Solution:
         #if number is in that set, append that to our ans arr
         #   if there is the same number, do not add it
         #
-        # if len(nums1) >= len(nums2):
-        #     shortest = nums2
-        #     longest = set(nums1)
-        # else:
-        #     shortest = nums1
-        #     longest = set(nums2)
-        # ans = []
-        # for num in shortest:
-        #     if num in longest: #if the number is also in longest
-        #         ans.append(num)
-        # return ans
-        #brute force
+        hashmap = {}
+        for num in nums1:
+            hashmap[num] = hashmap.get(num, 0) + 1
         ans = []
-        for i in range(len(nums1)):
-            for j in range(len(nums2)):
-                if nums2[j] == nums1[i]:
-                    ans.append(nums2[j])
-                    nums2.pop(j)
-                    break
+        for num in nums2:
+            if num in hashmap:
+                ans.append(num)
+                hashmap[num] -= 1
+                if hashmap[num] == 0:
+                    del hashmap[num]
         return ans
+        #brute force
+        # ans = []
+        # for i in range(len(nums1)):
+        #     for j in range(len(nums2)):
+        #         if nums2[j] == nums1[i]:
+        #             ans.append(nums2[j])
+        #             nums2.pop(j)
+        #             break
+        # return ans
