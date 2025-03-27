e59df045ab6194c1e3705985e164e86cb5e2f2cb
Daniel Phanachone <109443146+dp1p@users.noreply.github.com>
2025-03-27 16:57:38 -0500

    Time: 83 ms (18.7%), Space: 33.3 MB (74.86%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,27 +1,16 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        h = {} #to store the nums in the hashtable
-        counter = 1 #to keep track of the nums
-        nums = sorted(nums) #to sort the nums
-        print(nums)
-        if nums == []:
-            print('empty')
-            return 0
-        for i in range(len(nums)-1):
-            # print(f'CURRENT NUM: {nums[i]} || NEXT NUM: {nums[i+1]}')
-            nextNum = nums[i+1] #get the next num in the arr
-            if nums[i]+1 == nextNum: #if the next number increments
-                counter+=1
-                # print(f'COUNTER COUNT {counter}')
-            elif nums[i] == nums[i+1]: #if num is equal to the next one
-                # print(f'COUNTER COUNT STAYS THE SAME {counter}')
-                continue
-            else: #if the number does NOT increment
-                h[counter] = 0     #the key will the counter, value will just be zero.
-                counter = 1         #reset counter
-                # print(f'COUNTER RESET: {counter}')
+        nums = set(nums)
+        longest = 0
+        for num in nums:
+            streak = 1
+            if num-1 not in nums:
+                current = num
+                streak = 1
+
+                while current + 1 in nums:
+                    streak += 1
+                    current += 1
             
-        h[counter] = 0 #if the number keeps incrementing all the way to end of the arr
-
-        return max(h.keys()) #return the max value key
-
+            longest = max(streak, longest)
+        return longest
